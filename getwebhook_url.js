409073f686da4b19059cf0aa8892aeaf2f43c2dd
chadcarlson <chad.carlson@platform.sh>
2020-01-27 14:24:18 -0500

    Get url atrribute.

--- getwebhook_url.js
+++ getwebhook_url.js
@@ -3,4 +3,4 @@ const config = require("platformsh-config").config();
 // process.env["WEBHOOK_PROXY_URL"] = config.getPrimaryRoute();
 // const url = "https://pr-4-afnwgxy-62i3ojbvofnua.eu-3.platformsh.site";
 // console.log(config.getPrimaryRoute().url);
-console.log(config.getPrimaryRoute())
+console.log(config.getPrimaryRoute().url)
