a2eda89441a71fb30ea625cc91abca4d2f9bfa81
chadcarlson <chad.carlson@platform.sh>
2020-01-27 10:15:22 -0500

    Works, but grabbing wrong webhook-url for some reason.

--- main.js
+++
@@ -1,7 +0,0 @@
-// main.js
-const { Probot } = require('probot')
-const app = require('./index.js')
-// require('dotenv').config({ path: 'run/.env' })
-
-// pass a probot app as a function
-Probot.run(app)
