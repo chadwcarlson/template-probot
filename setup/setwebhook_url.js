be8479e3c26be0e8e415f586c5dd97a47fc29832
chadcarlson <chad.carlson@platform.sh>
2020-01-28 13:05:36 -0500

    Set dynamically.

--- setup/setwebhook_url.js
+++ setup/setwebhook_url.js
@@ -2,16 +2,31 @@ const yaml = require('js-yaml');
 const fs   = require('fs');
 const config = require("platformsh-config").config();
 
+
+const default_description = `# GitHub/Probot App Template for Platform.sh
+
+This is a basic template for setting up a production-ready GitHub App using Probot on Platform.sh.
+
+Once the template is initialized, the installer will direct you to register the app with GitHub. After that, it is set up to leave a welcome message comment on each newly opened issue for each repository it has been installed on.
+
+Afterwards, you can update the template to perform more actions using the GitHub API, including adding additional permissions.
+
+`;
+
 try {
-  var doc = yaml.safeLoad(fs.readFileSync('app.yml', 'utf8'));
-  doc.url = config.getPrimaryRoute().url;
+
+  var doc = yaml.safeLoad(fs.readFileSync('probot.app.yml', 'utf8'));
+
+  if (!("description" in doc)) {
+    console.log("No description defined in manifest. Using Platform.sh default description.");
+    doc.description = default_description;
+  }
+
+  doc.hook_attributes = { url: config.getPrimaryRoute().url };
+
   fs.writeFileSync('app.yml', yaml.safeDump(doc), function (err) {
     if (err) throw err;
   });
-  // var env = `WEBHOOK_PROXY_URL=${config.getPrimaryRoute().url}`;
-  // fs.writeFileSync('run/.env', env, function (err) {
-  //   if (err) throw err;
-  // });
 
 } catch (e) {
   console.log(e);
